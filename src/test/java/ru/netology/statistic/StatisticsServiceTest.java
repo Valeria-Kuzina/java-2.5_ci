6d7ffb8c5872786546bad22f5a7e2a89a11832bd
valeriiakuzina <valeriiakuzina@gmail.com>
2020-04-27 20:26:47 +0600

    second version homework without error

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatisticsServiceTest {
 
@@ -12,6 +12,18 @@ class StatisticsServiceTest {
 
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void useCurrentMax() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 14, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 14;
 
     long actual = service.findMax(incomesInBillions);
 
